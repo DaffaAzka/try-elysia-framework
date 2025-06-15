5c5a5544a2777a057e0355cc8ef01f399327cc77
Daffa Azka <azkadaiki26@gmail.com>
2025-06-15 07:51:02 +0700

    feat: create, update, and delete

--- src/note.ts
+++ src/note.ts
@@ -1,7 +1,29 @@
+import { Update } from './../node_modules/@sinclair/typebox/build/cjs/value/delta/delta.d';
 import { Elysia, status, t } from 'elysia'
 
-class Note{
+class Note {
     constructor(public data: string[] = ["Moonhalo"]) {}
+
+    add(note: string): string | any {
+        if (note != "") {
+            this.data.push(note)
+            return note;
+        } else {
+            return status("Gone")
+        }
+    }
+
+    update(note: string, index:number): string {
+        this.data[index] = note
+        return this.data[index]
+    }
+
+    delete(index: number): string[] {
+        return this.data.splice(index, 1)
+    }
+
+
+
 }
 
 export const note = new Elysia()
@@ -11,6 +33,36 @@ export const note = new Elysia()
     .get('/note/:index', ({note, params: {index}}) => {
         return note.data[index] ?? status(404)
     },
+    {
+        params: t.Object({
+            index: t.Number()
+        })
+    })
+    .patch('/note/:index', ({
+        note, params: {index}, body: {data}
+    }) => {
+        note.update(data, index)
+    },
+    {
+        body: t.Object({
+            data: t.String()
+        }),
+
+        params: t.Object({
+            index: t.Number()
+        })
+    })
+    .put('/note', ({ note, body: { data } }) => {
+        return note.add(data)
+    },
+    {
+        body: t.Object({
+            data: t.String()
+        })
+    })
+    .delete('/note/:index', ({note, params: {index}}) => {
+        return note.delete(index)
+    }, 
     {
         params: t.Object({
             index: t.Number()
